ba06f381d0790adf7b9b240d3be84ca6fb7ac1dd
Katerina Atanasoska <Katerina.Atanasoska@sedc.mk>
2020-03-02 20:53:43 +0100

    Cllass 1

---
+++ Class01/ConsoleApp-Class01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Class01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Test string ");
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("We are learning c#");
+
+            //Console.Write("Dejan ");
+            //Console.Write("Kristina");
+
+            //Console.WriteLine("    *");
+            //Console.WriteLine("   ***");
+            //Console.WriteLine("  *****");
+            //Console.WriteLine(" *******");
+            //Console.WriteLine("*********");
+
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine("  *   *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine(" **   **");
+            Console.ReadLine();
+        }
+    }
+}
