cfb2cb38607ad579ce50be416d7ba5a815846fb9
KaterinaAtanasoska <40323665+KaterinaAtanasoska@users.noreply.github.com>
2020-03-26 15:59:14 +0100

    Class3.2Homework

---
+++ Class3.1Homework/StudentGroup/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = new string[] { "Zoran", "Milan", "Tome", "Bogdan", "Stevan" };
+            string studentGroup;
+
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+            studentGroup = Console.ReadLine();
+
+            if (studentGroup != "2" && studentGroup != "1")
+            {
+                Console.WriteLine(studentGroup + " group does not exist");
+            }
+            else if (studentGroup == "1")
+            {
+                Console.WriteLine("The Students in G1 are: ");
+                foreach (string name in studentsG1)
+                {
+                    Console.Write(name + ", ");
+                }
+            }
+            else if (studentGroup == "2")
+            {
+                Console.WriteLine("The Students in G2 are: ");
+                foreach (string name in studentsG2)
+                {
+                    Console.Write(name + ", ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class3.1Homework/Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integerArray = new int[6];
+            int sum = 0;
+
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                Console.WriteLine("Please enter " + i + "th number in array ");
+                integerArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int number in integerArray)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine("The sum of even numbers in this array is: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
